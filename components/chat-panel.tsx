56d4d4abfc9a62102733774052a2155def051370
Eric Ngoiya <115367894+ngoiyaeric@users.noreply.github.com>
2024-04-04 19:29:25 +0300

    example

--- components/chat-panel.tsx
+++ components/chat-panel.tsx
@@ -37,25 +37,26 @@ export function ChatPanel({
   const exampleMessages = [
     {
       heading: 'What are the',
-      subheading: 'trending memecoins today?',
-      message: `What are the trending memecoins today?`
+      subheading: 'closest EV charging stations near me?',
+      message: `What are the closest EV charging stations near me?`
     },
     {
-      heading: 'What is the price of',
-      subheading: '$DOGE right now?',
-      message: 'What is the price of $DOGE right now?'
+      heading: 'When is the best time',
+      subheading: 'to play soccer next week',
+      message: 'When is the best time to play soccer next week?'
     },
     {
-      heading: 'I would like to buy',
-      subheading: '42 $DOGE',
-      message: `I would like to buy 42 $DOGE`
+      heading: 'Set up a meeting',
+      subheading: 'with Jane next week for a picnic',
+      message: `Set up a meeting with Jane next week for a picnic.`
     },
     {
-      heading: 'What are some',
-      subheading: `recent events about $DOGE?`,
-      message: `What are some recent events about $DOGE?`
+      heading: 'How much longer is it going to rain?',
+      subheading: `recent events the weather`,
+      message: `How much longer is it going to rain??`
     }
   ]
+
 
   return (
     <div className="fixed inset-x-0 bottom-0 w-full bg-gradient-to-b from-muted/30 from-0% to-muted/30 to-50% duration-300 ease-in-out animate-in dark:from-background/10 dark:from-10% dark:to-background/80 peer-[[data-state=open]]:group-[]:lg:pl-[250px] peer-[[data-state=open]]:group-[]:xl:pl-[300px]">
